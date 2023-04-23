f708237ef7b630dd35bfa33a6c51122a8629813e
heuremi <cvazqueztes@gmail.com>
2023-04-23 11:40:39 -0400

    cajita inventario 1

--- mysterious escape/Assets/Scripts/Inventory.cs
+++ mysterious escape/Assets/Scripts/Inventory.cs
@@ -2,16 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inventory {
-    private List<Item> itemList;
-    public Inventory() {
-        itemList = new List<Item>();
-
-        AddItem(new Item { itemType = Item.ItemType.Weapon, amount = 1 });
-
-    }
-
-    public void AddItem (Item item) {
-        itemList.Add(item);
-    }
+public class Inventory : MonoBehaviour {
+    public bool[] isFull;
+    public GameObject[] slots;
 }

8f7094de2e03b9af78d0261079d6f512849dee47
heuremi <cvazqueztes@gmail.com>
2023-06-14 19:38:02 -0400

    inventario y agarrar objetos

---
+++ mysterious escape/Assets/Scripts/Pickup.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private Inventory inventory;
+    public GameObject itemButton;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            for(int i = 0; i < inventory.slots.Length; i++)
+            {
+                if (inventory.isFull[i] == false)
+                {
+                    //se puede agregar el item al inventario
+                    inventory.isFull[i] = true;
+                    Instantiate(itemButton, inventory.slots[i].transform);
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+    }
+}

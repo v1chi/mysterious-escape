7d7d30b1761a2256414699321178ddedc52a6be4
feliS317 <felipesalazar@vtr.net>
2023-06-20 12:45:34 -0400

    Arreglado ataque de jugador
    + Juntadas todas las hitbox en una, y alterada la forma de la hitbox mediante las animaciones
    + Cambiado nombre de función "OnTriggerEnter2d" a "OnTriggerEnter2D" (porque ese el nombre correcto, de lo contrario no funciona)
    - Algo a considerar es que el enemigo muere instantáneamente, ya que el OnTriggerEnter2D chequea por cada frame si tiene algo adentro, y como el enemigo no tiene ningún cooldown para recibir daño, en 2 frames se muere

--- mysterious escape/Assets/Scripts/HurtEnemy.cs
+++ mysterious escape/Assets/Scripts/HurtEnemy.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class HurtEnemy : MonoBehaviour
 {
     public int damageToGive = 2;
+    private BoxCollider2D hitbox;
     // Start is called before the first frame update
     void Start()
     {
-        
+        hitbox = GetComponent<BoxCollider2D>();
+        hitbox.enabled = false;
     }
 
     // Update is called once per frame
@@ -17,10 +19,12 @@ public class HurtEnemy : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2d(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("Colision detectada");
         if (other.tag == "Enemy")
         {
+            Debug.Log("Enemigo detectado");
             other.GetComponent<EnemyHealth>().HurtEnemy(damageToGive);
         }
     }
